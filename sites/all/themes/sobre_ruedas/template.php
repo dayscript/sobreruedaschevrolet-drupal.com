e429e4e48a77c5dd1f4bcacffcb46ba5216b4205
Edwin Vega <edwin.vega.moreno@gmail.com>
2017-02-08 23:38:44 +0000

    add modules and libraries

---
+++ sites/all/themes/sobre_ruedas/template.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function sobre_ruedas_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function sobre_ruedas_preprocess_page(&$variables) {
+}
+
+/*function sobre_ruedas_theme() {
+  $items = array();
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+  'render element' => 'form',
+  'path' => drupal_get_path('theme', 'sobre_ruedas') . '/templates',
+  'template' => 'user-login',
+  'preprocess functions' => array(
+  'sobre_ruedas_user_login'
+  ),
+ );
+return $items;
+}*/
