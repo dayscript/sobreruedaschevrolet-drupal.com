37d50b243927061e2b1b87b86263e11dff5454de
Edwin Vega <edwin.vega.moreno@gmail.com>
2017-02-09 19:17:41 +0000

    Adjustments on the template

--- sites/all/themes/sobre_ruedas/template.php
+++ sites/all/themes/sobre_ruedas/template.php
@@ -11,17 +11,3 @@ function sobre_ruedas_preprocess_html(&$variables) {
  */
 function sobre_ruedas_preprocess_page(&$variables) {
 }
-
-/*function sobre_ruedas_theme() {
-  $items = array();
-  // create custom user-login.tpl.php
-  $items['user_login'] = array(
-  'render element' => 'form',
-  'path' => drupal_get_path('theme', 'sobre_ruedas') . '/templates',
-  'template' => 'user-login',
-  'preprocess functions' => array(
-  'sobre_ruedas_user_login'
-  ),
- );
-return $items;
-}*/
