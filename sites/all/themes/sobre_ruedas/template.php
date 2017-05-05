57938abf39060c4e8237457cf59d78a6d73fd0af
Juan Carlos Orrego <jcorrego@gmail.com>
2017-05-05 09:06:20 -0500

    merge

--- sites/all/themes/sobre_ruedas/template.php
+++ sites/all/themes/sobre_ruedas/template.php
@@ -10,4 +10,16 @@ function sobre_ruedas_preprocess_html(&$variables) {
  * Implements template_preprocess_page.
  */
 function sobre_ruedas_preprocess_page(&$variables) {
+  $user = $variables['user'];
+
+  $show_contextual_tabs = FALSE;
+  foreach ($user->roles as $role) {
+    if($role == 'administrator'){
+      $show_contextual_tabs = TRUE;
+    }
+  }
+
+  if(!$show_contextual_tabs){
+    unset($variables['tabs']);
+  }
 }
