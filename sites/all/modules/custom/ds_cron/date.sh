4f898a03fff1cbc5727ee17a454ad3531e36c675
ivandelvallelaestacion <ivan.delvalle@grupolaestacion.com>
2017-11-07 16:47:46 -0500

    ajuste de pruebas en job

--- sites/all/modules/custom/ds_cron/date.sh
+++ sites/all/modules/custom/ds_cron/date.sh
@@ -10,7 +10,7 @@ echo $post_date;
 while true; do
   echo '---';
   post_date=`date '+%Y%m%d' -d "$date+1 days"`;
-  wget "http://sobreruedaschevrolet.local/ds_cron/purchases/test?date_start=$date&date_end=$post_date"
+  wget "http://sobreruedaschevrolet.com/ds_cron/purchases/test?date_start=$date&date_end=$post_date"
   date=$post_date;
   echo '---';
 done
