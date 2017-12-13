d576065f951eff89f6aa785151740de68cebfc7b
Your Name <idelvalle@grupo-link.com>
2017-12-13 21:21:32 +0000

    Story 182: Fix benchmarks for speed

--- sites/all/modules/custom/ds_cron/date.sh
+++ sites/all/modules/custom/ds_cron/date.sh
@@ -1,16 +1,17 @@
 today=$(date +"%Y%m%d");
 
-date='20170801';
-post_date=`date '+%Y%m%d' -d "$date+28 days"`;
-
+#today='20170802';
+echo $today;
+date=`date '+%Y%m%d' -d "$today-2 days"`;
 echo $date;
+post_date=`date '+%Y%m%d' -d "$date+1 days"`;
 echo $post_date;
 
-
-while true; do
+#while true; do
   echo '---';
-  post_date=`date '+%Y%m%d' -d "$date+28 days"`;
-  wget "http://sobreruedaschevrolet.com/ds_cron/purchases/test?date_start=$date&date_end=$post_date"
+#  post_date=`date '+%Y%m%d' -d "$date+1 days"`;
+  wget "http://sobreruedaschevrolet.com/ds_cron/purchases/test?date_start=$date&date_end=$today"
   date=$post_date;
   echo '---';
-done
+#done
+
